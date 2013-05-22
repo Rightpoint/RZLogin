5ccc3ab099d747c22b337f200f96e37a658a82dd
Daniel Kopyc <daniel@sfjava.net>
2013-05-22 15:51:41 -0400

    added detailed comments to top of RZLoginViewController, with example basic usage of class, and notes/caveats re: customization

--- RZLogin/View Controllers/RZLoginViewController.h
+++ RZLogin/View Controllers/RZLoginViewController.h
@@ -2,6 +2,63 @@
 //  RZLoginViewController.h
 //  RZLogin
 //
+//  RZLoginViewController is a UIViewController that encapsulates functionality for a "user login" experience, and allows them
+//  to choose between several supported login 'types' (e.g. login with an email-address/password; login with Facebook or Twitter, etc.)
+//
+//  This class can be sub-classed to override defaults or implement custom functionality; the use of alternate NIB/XIB files also allows
+//  for complete customization of all of the login views. Please refer to the examples in this project for details.
+//
+//  The delegate, which (depending on which login-types your app wishes to support),
+//  must conform to one or more of the following protocols:
+//
+//      RZLoginEmailViewControllerDelegate -- for login with an email-address and password,
+//      RZFacebookViewControllerDelegate   -- for login via Facebook account,
+//      RZTwitterViewControllerDelegate    -- for login via Twitter account.
+//
+//  In the simplest use case, one can simply create a (default) instance of RZLoginViewController,
+//  and push it onto the current nav-stack, for example like so:
+//
+//  @implementation MyRootViewController
+//     ...
+//
+//  - (void) doLogin {
+//
+//      RZLoginViewController *loginController = [[RZLoginViewController alloc] init];
+//      loginController.delegate = self;
+//
+//      [self.navigationController pushViewController:loginController animated:YES];
+//  }
+//
+//  The delegate (typically 'self') can then simply implement the various properties and methods
+//  required by each of the login-type protocols that it chooses to  support. For example, to support
+//  login via Facebook, we would implement the protocol for RZLoginFacebookViewControllerDelegate, like so:
+//
+//  @interface MyRootViewController () <RZLoginFacebookViewControllerDelegate>
+//     ...
+//
+//  @implementation MyRootViewController
+//     ...
+//
+//    - (NSString *)facebookAppId {
+//        
+//        return @"351055245000574";
+//    }
+//
+//    - (void)didLoginWithFacebookWithToken:(NSString *)fbToken fullName:(NSString *)fullName userId:(NSString *)userId
+//    {
+//        NSLog(@"%s: %@ - %@", __FUNCTION__, fullName, fbToken);
+//    }
+//
+//  @end
+//
+//  ---------------------------------------------------------------------------------
+//  Further notes on the RZLoginEmailViewController and RZSignUpViewController forms:
+//
+//  In addition, note that if you're going to customize either the email login or sign-up forms, you should be aware of
+//  the |formFields| IBOutletCollection, and the action methods required when creating a custom XIB for those forms.
+//  For those forms, each text-field should also have either a unique placeholder-text value or a unique view 'tag',
+//  since these are used by the form-field validators (RZValidator).
+//
 //  Copyright (c) 2013 Raizlabs. All rights reserved.
 //
 #import <UIKit/UIKit.h>
@@ -31,13 +88,8 @@
 @property (nonatomic, assign, getter=isForgotPasswordAllowed) BOOL forgotPasswordAllowed;
 @property (nonatomic, assign, getter=shouldPresentViewsAsModal) BOOL presentViewsAsModal; // else, present via 'push' onto nav-controller
 
-// the delegate, which (depending on which login-types your app wishes to support),
-// must conform to one or more of the following protocols:
-// 
-//      RZLoginEmailViewControllerDelegate -- for login with an email-address and password,
-//      RZFacebookViewControllerDelegate   -- for login via Facebook account,
-//      RZTwitterViewControllerDelegate    -- for login via Twitter account.
-//
+// the delegate (see above for details) must conform to one or more of the following protocols:
+//  RZLoginEmailViewControllerDelegate, RZFacebookViewControllerDelegate, RZTwitterViewControllerDelegate
 @property (nonatomic, weak) id delegate; 
 
 @property (nonatomic, strong) RZLoginEmailViewController *emailLoginViewController;
